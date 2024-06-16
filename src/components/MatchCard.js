47f9e602c7172f933b453f804ebe1501fb2f91b3
Yoryoboy <93jads@gmail.com>
2024-06-16 15:36:03 -0300

    Finish css module part 4

--- src/components/MatchCard.js
+++ src/components/MatchCard.js
@@ -1,10 +1,11 @@
 import React from "react";
-import "./MatchCard.css";
 import MatchItem from "./MatchItem";
+
+import styles from "./MatchCard.module.css";
 
 function MatchCard({ title, buttons }) {
   return (
-    <section className="upcoming-matches">
+    <section className={styles.upcomingMatches}>
       <h2>{title}</h2>
       <MatchItem buttons={buttons} />
     </section>
